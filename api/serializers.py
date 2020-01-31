8c0e9900a67116f408ef957e1608879eeba9acfb
ordizzz <ordizzz2008@yandexru>
2020-01-31 23:10:58 +0300

    Some small fixes

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from rest_framework.serializers import OrderedDict
 
 class ItemSerializer(serializers.Serializer):
-    item_number = serializers.IntegerField(),
+    item_number = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
     category = serializers.CharField(max_length=15)
     type = serializers.CharField(max_length=30)
+    group = serializers.CharField(max_length=30)
     standard = serializers.FloatField()
     standard_hc = serializers.FloatField()
     current = serializers.FloatField()
